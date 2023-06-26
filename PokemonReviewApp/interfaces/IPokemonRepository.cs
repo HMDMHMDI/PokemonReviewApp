0eed79f79ebc7da2a63550470eb9c5b31468f3cb
HAMED <130986160+HMDMHMDI@users.noreply.github.com>
2023-06-26 11:21:03 +0330

    First Web Api
    Models.
    Migration.
    SeedData.
    Interface and Repository.
    Data Context.

---
+++ PokemonReviewApp/interfaces/IPokemonRepository.cs
@@ -0,0 +1,12 @@
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.interfaces;
+
+public interface IPokemonRepository
+{
+    ICollection<Pokemon> GetPokemons();
+    Pokemon GetPokemon(int id);
+    Pokemon GetPokemon(string name);
+    decimal GetPokemonRating(int pokeId);
+    bool PokemonExists(int pokeId); 
+}
